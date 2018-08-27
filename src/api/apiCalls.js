c2fe79cde25dd7784c326e73723c641525b30597
Joel O'Connor <joconno07@gmail.com>
2018-08-27 14:39:42 -0600

    added in git ignore and api info

---
+++ src/api/apiCalls.js
@@ -0,0 +1,1 @@
+const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018`;
