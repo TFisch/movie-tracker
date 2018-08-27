9fef3b18545a3b858b38feee2af26c11bdd62cc9
Tim Fischer <emailtimfischer@gmail.com>
2018-08-27 15:16:26 -0600

    Create fetchMovies to make initial fetch

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,1 +1,8 @@
 const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018`;
+
+export const fetchMovies = async () => {
+  let resolvedMovies;
+  const response = await fetch(url);
+  const data = await response.json();
+  return (resolvedMovies = await getMovies(data));
+}
