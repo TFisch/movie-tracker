ac2572844521c09b6e46b03a630cb343b28b4a97
Tim Fischer <emailtimfischer@gmail.com>
2018-08-27 15:23:47 -0600

    Create helper file

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,3 +1,6 @@
+import { getMovies } from '../helper';
+
+
 const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018`;
 
 export const fetchMovies = async () => {
