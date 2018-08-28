0f2cc9f8f2ed958b18a9939674667c17e9f5969e
Joel O'Connor <joconno07@gmail.com>
2018-08-27 18:00:00 -0600

    setup store in app for redux

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './containers/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-// import { rootReducer } from './reducers';/
+import { rootReducer } from './reducers';
 import { BrowserRouter } from 'react-router-dom';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__();
 
-// const store = createStore(rootReducer, devTools);
+const store = createStore(rootReducer, devTools);
 const router = (
-  <Provider >
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
