416fb7b1d5a2f483c14e67c57de62218a4d20573
Tim Fischer <emailtimfischer@gmail.com>
2018-08-27 19:12:47 -0600

    Render title to card

--- src/components/Card/Index.js
+++ src/components/Card/Index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const Card = () => {
+export const Card = ({ title }) => {
 
   return (
     <div className="card">
-
+      <h1>{title}</h1>
     </div>
   )
 }
 
-export default Card;
