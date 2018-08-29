b614c9965b1c2d9899915868940ac92fdc66726b
Joel O'Connor <joconno07@gmail.com>
2018-08-29 17:46:02 -0600

    update to card props

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './style.css';
 import StarRatings from 'react-star-ratings';
 
-export const Card = ({ title, poster_path, vote_average }) => {
+export const Card = ({ poster_path, vote_average }) => {
   const rating = vote_average / 2;
   return (
     <div className="card">
-      <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} />
+      <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} alt='movie data' />
       <StarRatings starDimension={'1em'} rating={rating} numberOfStars={5} starRatedColor={'gold'} />
     </div>
   );
 };
-
-export default Card;
