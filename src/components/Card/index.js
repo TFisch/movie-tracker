47dd2c47d7bbbd44f46d99980666ff5fab35d909
Joel O'Connor <joconno07@gmail.com>
2018-08-29 14:05:49 -0600

    updated card to display poser

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './style.css';
 
-export const Card = ({ title }) => {
+export const Card = ({ title, poster_path }) => {
 
   return (
     <div className="card">
-      <h1>{title}</h1>
+      <img src={`https://image.tmdb.org/t/p/w200${poster_path}`}/>
     </div>
   );
 };
