8f4919f2460348edd2ad7ba49c37ba6ad7eb9669
Tim Fischer <emailtimfischer@gmail.com>
2018-08-29 14:25:18 -0600

    Build out post object

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,9 +1,20 @@
 import { apiKey } from '../api/apiKey';
 
-const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018`;
 
 export const fetchMovies = async () => {
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018`;
   const response = await fetch(url);
   const data = await response.json();
   return data.results;
 };
+
+export const login = async ({ email, password }) => {
+  const url = 'http://localhost:3000/api/users'
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ email, password }),
+    headers: { 'Content-Type': 'application/json' }
+  });
+  const data = await response.json();
+  return data.results;
+}
