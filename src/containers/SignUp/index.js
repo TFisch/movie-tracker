4fa4f58d9e2b470429a1b2a4691cea1902a3cd69
Tim Fischer <emailtimfischer@gmail.com>
2018-08-29 15:15:36 -0600

    Add type of password to input field

--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -30,7 +30,7 @@ export class SignUp extends Component {
       <form className='user-login' onSubmit={this.handleSubmit}>
         <input name='userName' placeholder='username' value={userName} onChange={this.handleChange} />
         <input name='email' placeholder='email' value={email} onChange={this.handleChange} />
-        <input name='password' placeholder='password' value={password} onChange={this.handleChange} />
+        <input name='password' placeholder='password' type='password' value={password} onChange={this.handleChange} />
         <button>SignUp</button>
       </form>
     );
