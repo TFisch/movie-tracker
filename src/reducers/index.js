21f9982d6928bfdffc76b6e7dbc11d99ec1865ee
Tim Fischer <emailtimfischer@gmail.com>
2018-08-30 14:13:27 -0600

    Add userData to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { moviesReducer } from '../reducers/moviesReducer';
+import { userReducer } from '../reducers/userReducer'
 
 export const rootReducer = combineReducers({
-  moviesData: moviesReducer
+  moviesData: moviesReducer,
+  userData: userReducer
 });
