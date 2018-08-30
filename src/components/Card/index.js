ed56eac5ae7277ffda6e922d0bf731a473974f3f
Tim Fischer <emailtimfischer@gmail.com>
2018-08-30 14:11:28 -0600

    Post user favorites

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './style.css';
 import StarRatings from 'react-star-ratings';
+import { postFavorites } from '../../api/apiCalls';
 
-export const Card = ({ poster_path, vote_average }) => {
+export const Card = ({ movie }) => {
+  const { poster_path, vote_average } = movie
   const rating = vote_average / 2;
   return (
-    <div className="card">
-      <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} alt='movie data' />
+    <div className='card'>
+      <button>FAVORITE</button>
+      <img className='poster-image' src={poster_path} alt='movie data' />
       <StarRatings starDimension={'1em'} rating={rating} numberOfStars={5} starRatedColor={'gold'} />
     </div>
   );
