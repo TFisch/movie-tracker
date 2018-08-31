b72f3711862d9be9c707fd9354d7303fbd5c3772
Joel O'Connor <joconno07@gmail.com>
2018-08-31 16:55:50 -0600

    Added action type to reducer to handle get user favorites

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -2,6 +2,8 @@ export const favoritesReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_FAVORITE':
       return [action.movie, ...state];
+    case 'SET_USER_FAVORITES':
+      return action.favorites;
     default:
       return state;
   }
