ce05fd5a7b151cca4a46a02cf77835db09835b48
Joel O'Connor <joconno07@gmail.com>
2018-08-30 18:17:19 -0600

    Moved NavBar to component directory. Moved Card to container directory

--- src/containers/CardContainer/index.js
+++ src/containers/CardContainer/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Card from '../../components/Card';
+import Card from '../../containers/Card';
 import './style.css';
 
 const CardContainer = (props) => {
