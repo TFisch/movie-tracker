c638816889327f2994c3ea4d959921ec17f93a6f
Joel O'Connor <joconno07@gmail.com>
2018-09-01 22:32:35 -0600

    Connected NavBar to store and built out most logic for user navigation

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { resetTheStore } from '../../actions';
 import './style.css';
 
-export const NavBar = () => {
+const NavBar = (props) => {
+  const { resetTheStore } = props;
+  const { name } = props.user;
   const noLoggedInNav = () => (
     <div>
       <Link to='/login'><button className='button'>Login</button></Link>
@@ -12,16 +17,31 @@ export const NavBar = () => {
 
   const loggedInNav = () => (
     <div>
-      <h1>Welcome! User</h1>
-      <Link to='/user/favorites'><button className='button'>Favorites</button></Link>
-      <Link to='/'><button className='button'>Logout</button></Link>
+      <h1>{`Welcome! ${name}`}</h1>
+      <Link to={`/${name}/favorites`}><button className='button'>Favorites</button></Link>
+      <Link to='/'><button onClick={resetTheStore} className='button'>Logout</button></Link>
     </div>
   );
 
   return (
     <div className='nav-bar'>
       <Route exact path='/' component={noLoggedInNav}></Route>
-      <Route path='/user' component={loggedInNav}></Route>
+      <Route path={`/${name}`} component={loggedInNav}></Route>
     </div>
   );
 };
+
+const mapStateToProps = (state) => ({
+  user: state.userData
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  resetTheStore: () => dispatch(resetTheStore())
+});
+
+NavBar.propTypes = {
+  user: PropTypes.object,
+  resetTheStore: PropTypes.func
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
