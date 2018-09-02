b406d8876e42fec6a6f084a78e5fe6f72de096d6
Joel O'Connor <joconno07@gmail.com>
2018-09-01 22:33:51 -0600

    Updated user reducer for store reset on user logout

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,10 @@
 export const userReducer = (state = {}, action) => {
+  console.log(action);
   switch (action.type) {
     case 'SET_ACTIVE_USER':
       return { user_id: action.user_id, name: action.name };
+    case 'RESET_THE_STORE':
+      return {};
     default:
       return state;
   }
