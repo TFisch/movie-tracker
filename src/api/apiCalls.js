e789131530c69424d4f541d8f2c55ce89337936e
Tim Fischer <emailtimfischer@gmail.com>
2018-09-02 11:37:15 -0600

    Add try catch to cleanMovies

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,31 +1,39 @@
 import { apiKey } from '../api/apiKey';
 
 export const fetchMovies = async () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return cleanMoviesData(data.results);
-};
+  try {
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return cleanMoviesData(data.results);
+  } catch (error) {
+    console.log("error", error)
+  }
+}
+
 
 export const cleanMoviesData = async (movies) => {
-  return movies.map(movie => {
-    const { id, title, poster_path, release_date, vote_average, overview } = movie;
-    const fullPosterPath = `https://image.tmdb.org/t/p/w200${poster_path}`;
-    return (
-      {
-        movie_id: id,
-        title,
-        poster_path: fullPosterPath,
-        release_date,
-        vote_average,
-        overview
-      }
-    );
-  });
+  try {
+    return movies.map(movie => {
+      const { id, title, poster_path, release_date, vote_average, overview } = movie;
+      const fullPosterPath = `https://image.tmdb.org/t/p/w200${poster_path}`;
+      return (
+        {
+          movie_id: id,
+          title,
+          poster_path: fullPosterPath,
+          release_date,
+          vote_average,
+          overview
+        }
+      );
+    });
+  } catch (error) {
+    console.log('error', error);
+  }
 };
 
 export const login = async ({ email, password }) => {
-  console.log('heelp')
   const url = 'http://localhost:3000/api/users';
   const response = await fetch(url, {
     method: 'POST',
