2f717ab68ac5430c2eda6bf0a56120606d79108e
Tim Fischer <emailtimfischer@gmail.com>
2018-09-01 21:17:16 -0600

    Complete fetch data test

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -4,7 +4,6 @@ export const fetchMovies = async () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
   return cleanMoviesData(data.results);
 };
 
--- src/test/apiCalls.test.js
+++ src/test/apiCalls.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { fetchMovies } from '../api/apiCalls';
+import { mockFullData } from '../test/mockData'
 
-describe('fetchMovies', () => {
+describe('fetchMovies', async () => {
   it('should fetch the intial movie data', () => {
-
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(mockFullData)
+    }))
+    const expected = 'https://api.themoviedb.org/3/discover/movie?api_key=45850ad06ddc2c2c2530d35aa8981c6c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018';
+    fetchMovies();
+    expect(window.fetch).toHaveBeenCalledWith(expected)
   })
 })
