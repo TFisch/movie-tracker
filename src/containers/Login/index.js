3d394d292f7e4e08005792727e643c4c06b1c9ed
Joel O'Connor <joconno07@gmail.com>
2018-09-01 22:32:57 -0600

    Built Login so that user will be redirected on login

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { setActiveUser, setUserFavorites } from "../../actions";
 import { login, getFavorites } from '../../api/apiCalls';
-import { Link, } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import './style.css';
 
 export class Login extends Component {
@@ -11,7 +11,8 @@ export class Login extends Component {
     super();
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      fireRedirect: false
     };
   }
 
@@ -27,38 +28,48 @@ export class Login extends Component {
     this.props.setActiveUser(user);
     const userFavorites = await getFavorites(user);
     this.props.setUserFavorites(userFavorites);
-    this.setState({ userName: '', password: '' });
+    this.setState({ userName: '', password: '', fireRedirect: true });
   }
 
   render() {
-
-    const { email, password } = this.state;
+    const { name } = this.props.user;
+    const { email, password, fireRedirect } = this.state;
     return (
-      <form className='user-login' onSubmit={this.handleSubmit}>
-        <h2 className='log-header'>Log In</h2>
-        <input
-          className='email input-field'
-          name='email'
-          placeholder='email'
-          type='email'
-          value={email}
-          onChange={this.handleChange}
-        />
-        <input
-          className='password input-field'
-          name='password'
-          placeholder='password'
-          type='password'
-          value={password}
-          onChange={this.handleChange}
-        />
-        <Link to={`/user/`} >
+      <div>
+        <form className='user-login' onSubmit={this.handleSubmit}>
+          <h2 className='log-header'>Log In</h2>
+          <input
+            className='email input-field'
+            name='email'
+            placeholder='email'
+            type='email'
+            value={email}
+            onChange={this.handleChange}
+          />
+          <input
+            className='password input-field'
+            name='password'
+            placeholder='password'
+            type='password'
+            value={password}
+            onChange={this.handleChange}
+          />
           <button className="log-button input-field">Log In</button>
-        </Link>
-      </form>
+          <Link to='/signup'>
+            <button className="log-button input-field">Sign UP</button>
+          </Link>
+        </form>
+        {fireRedirect && (
+          <Redirect to={`/${name}`}/>
+        )}
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  user: state.userData
+});
 
 const mapDispatchToProps = (dispatch) => ({
   setActiveUser: (user) => dispatch(setActiveUser(user)),
@@ -66,8 +77,9 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 Login.propTypes = {
+  user: PropTypes.object,
   setActiveUser: PropTypes.func,
   setUserFavorites: PropTypes.func
 };
 
-export default connect(null, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
