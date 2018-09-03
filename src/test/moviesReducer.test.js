c63489b07e3c71a3deca6a504278a0b1f74f9be0
Joel O'Connor <joconno07@gmail.com>
2018-09-03 11:56:30 -0600

    Reworked movieReducer test to use mockData

--- src/test/moviesReducer.test.js
+++ src/test/moviesReducer.test.js
@@ -1,22 +1,17 @@
-import { moviesReducer } from '../reducers/moviesReducer'
-import * as actions from '../actions'
+import { moviesReducer } from '../reducers/moviesReducer';
+import * as mockData from '../test/mockData';
+import * as actions from '../actions';
 
 describe('moviesReducer', () => {
+  const { mockCleanMovieData } = mockData;
+  const { addMovies } = actions;
   it('should return the default state', () => {
-    const expected = []
-    const result = moviesReducer(undefined, {})
-
-    expect(result).toEqual(expected)
-  })
+    const expected = [];
+    const result = moviesReducer(undefined, {});
+    expect(result).toEqual(expected);
+  });
 
   it('should return state with the top twenty movies', () => {
-    const mockMovieData = [
-      { title: 'Good Will Hunting', poster_path: '/g4oih2nkfs/sdaf3klj.jpg', release_date: '1998-02-17' },
-      { title: 'Bourne Supremacy', poster_path: '/l1orojfjfs/sdaf3klj.jpg', release_date: '2001-03-22' },
-      { title: 'Legend of Bagger Vance', poster_path: '/g4qwepokfs/saf23lj.jpg', release_date: '2004-11-06' }
-    ]
-
-    expect(moviesReducer(undefined, actions.addMovies(mockMovieData))).toEqual(mockMovieData)
-
-  })
-})
+    expect(moviesReducer(undefined, addMovies(mockCleanMovieData))).toEqual(mockCleanMovieData);
+  });
+});
