87a7d28865775f55a7a4faeb580292cb7d9a08b3
Tim Fischer <emailtimfischer@gmail.com>
2018-09-03 14:20:08 -0600

    Set mock activeUser and UserFavorites

--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Login } from '../containers/Login';
 import { shallow } from 'enzyme';
 import * as mockData from '../test/mockData';
+import { login } from '../api/apiCalls'
 
 describe('Login tests', () => {
   let wrapper;
@@ -21,18 +22,28 @@ describe('Login tests', () => {
   describe('handleSubmit', () => {
     let wrapper;
     let mockEvent;
+    let mockLogin;
+    let mockSetActiveUser;
+    let mockSetUserFavorites;
 
     beforeEach(() => {
+      mockEvent = { preventDefault: jest.fn(), target: { email: '', password: '' } }
+      mockLogin = jest.fn();
+      mockSetActiveUser = jest.fn();
+      mockSetUserFavorites = jest.fn();
+
       wrapper = shallow(
         <Login
           user={mockUserData}
+          setActiveUser={mockSetActiveUser}
+          mockSetUserFavorites={mockSetUserFavorites}
         />
-        mockEvent = { preventDefault: jest.fn(), target: { email: '', password: '' } }
       );
     });
 
     it('should setActive user when recieving a valid email and password', () => {
       wrapper.find('.user-login').simulate('submit', mockEvent)
+      expect(mockLogin).toHaveBeenCalled();
 
     })
 
