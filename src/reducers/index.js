dfafdfc39c6bac21e7423c94e4c0f19aba849387
Tim Fischer <emailtimfischer@gmail.com>
2018-10-06 17:37:29 -0600

    add Login reducer to root

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,14 @@ import { moviesReducer } from '../reducers/moviesReducer';
 import { userReducer } from '../reducers/userReducer';
 import { favoritesReducer } from '../reducers/favoritesReducer';
 import { selectedReducer } from '../reducers/selectedReducer';
+import { loginReducer } from '../reducers/loginReducer';
+
 
 export const rootReducer = combineReducers({
   moviesData: moviesReducer,
   userFavorites: favoritesReducer,
   userData: userReducer,
-  selectedMovie: selectedReducer
+  selectedMovie: selectedReducer,
+  activeLogin: loginReducer
+
 });
